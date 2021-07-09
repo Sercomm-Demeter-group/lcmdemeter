4c9b73ddb80d2fbdab9a10dfc0b9edaf7308dac2
Cloud Wu <cloud_wu@sercomm.com>
2021-07-09 20:54:40 +0800

    HTTP 404 is determined by more reasonable way in class ResourceFilter

--- openfire.plugin.demeter_service_api/src/java/com/sercomm/openfire/plugin/service/filter/ResourceFilter.java
+++ openfire.plugin.demeter_service_api/src/java/com/sercomm/openfire/plugin/service/filter/ResourceFilter.java
@@ -1,29 +1,39 @@
 package com.sercomm.openfire.plugin.service.filter;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.ext.Provider;
 
 @Provider
 public class ResourceFilter implements ContainerResponseFilter
 {
+    @Context 
+    private UriInfo uriInfo;
+
     @Override
     public void filter(
         ContainerRequestContext requestContext, 
         ContainerResponseContext responseContext)
     throws IOException 
     {
-        Object object = responseContext.getEntity();
-        // since Jersey responds HTTP 204 by default if no response entity is available,
+        List<Object> matchedClasses = uriInfo.getMatchedResources();
+
+        // since Jersey responds HTTP 204 by default if no request handler is available,
         // instead of HTTP 204, response code is desired to be HTTP 404
-        if(object == null)
+        if(matchedClasses.size() == 0)
         {
-            if(responseContext.getStatus() == Response.Status.NO_CONTENT.getStatusCode())
+            // in this case, HTTP 401 and HTTP 405 should not be changed
+            if(responseContext.getStatus() != Response.Status.UNAUTHORIZED.getStatusCode() &&
+               responseContext.getStatus() != Response.Status.METHOD_NOT_ALLOWED.getStatusCode())
             {
+                // respond HTTP 404
                 responseContext.setStatus(Response.Status.NOT_FOUND.getStatusCode());
             }
         }
